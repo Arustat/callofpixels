cf2d0d6c70274d468dcf8d937411075caeaf68de
Raaaaff <144777830+Raaaaff@users.noreply.github.com>
2024-11-25 15:40:54 +0100

    SQL Services

---
+++ SQL/GameTableClasses.cs
@@ -0,0 +1,11 @@
+namespace SQL;
+
+// Classe Gestion Pixels
+
+public class Pixel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Cos { get; set; }
+    public string Color { get; set; }
+}
--- SQL/Program.cs
+++ SQL/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Dapper;
+using MySql.Data.MySqlClient; // Utiliser MySqlConnection
 
-Console.WriteLine("Hello, World!");
+public class SqlServices
+{
+    // Chaîne de connexion fixe à la base de données 
+    private readonly string _connectionString = "Server=localhost;Port=3306;Database=callofpixel;Uid=root;";
+
+    // Fonction d'ajout de Pixel
+    public void AddPixel(string name, string cos, string color)
+    {
+        using (var connection = new MySqlConnection(_connectionString)) // Utilisation de MySqlConnection
+        {
+            Console.WriteLine("Connecting...");
+            connection.Open();
+            Console.WriteLine("Connected");
+
+            var sql = "INSERT INTO jeu1 (Name, Cos, Color) VALUES (@Name, @Cos, @Color)";
+            connection.Execute(sql, new { Name = name, Cos = cos, Color = color });
+
+            Console.WriteLine($"{name}'s Pixel with {color} color added in position {cos}");
+        }
+    }
+}
+
+class SQLProgram
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("CallofPixels SQL Communication Service");
+        Console.WriteLine("================================\n");
+        
+        // Créer une instance de SqlServices pour la connexion à la DB
+        
+        var pixelService = new SqlServices();
+        
+        pixelService.AddPixel("RougeRaph", "15,41", "#3ed63c");
+        pixelService.AddPixel("May", "53,124", "#ff6e4f");
+    }
+}
