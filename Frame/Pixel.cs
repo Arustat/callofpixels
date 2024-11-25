7a185c4d09a8151348e38f1fe4fb51c7835492cf
MayVCODE <114690973+Arustat@users.noreply.github.com>
2024-11-25 17:08:09 +0100

    Suicide

---
+++ Frame/Pixel.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Frame
+{
+    public class Pixel
+    {
+        public Rectangle Rect { get; set; } // Rectangle représentant le pixel
+        public SolidColorBrush Color { get; set; } // Couleur du pixel
+        public int X { get; set; } // Coordonnée X du pixel
+        public int Y { get; set; } // Coordonnée Y du pixel
+
+        // Constructeur de la classe Pixel
+        public Pixel(int x, int y)
+        {
+            X = x;
+            Y = y;
+            Color = new SolidColorBrush(Colors.White); // Par défaut, la couleur est blanche
+            Rect = new Rectangle
+            {
+                Width = 10,
+                Height = 10,
+                Fill = Color,
+                Stroke = new SolidColorBrush(Colors.Gray),
+                StrokeThickness = 0.5
+            };
+        }
+    }
+}
