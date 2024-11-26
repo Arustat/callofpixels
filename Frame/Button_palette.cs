671d1556b909c5794e1ad3ae05f8acdb92407bfb
MayVCODE <114690973+Arustat@users.noreply.github.com>
2024-11-26 15:01:32 +0100

    Color palette

---
+++ Frame/Button_palette.cs
@@ -0,0 +1,56 @@
+﻿//Classe Button qui va régir le button
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Frame;
+
+public class ColorPalette: Form
+{
+    private Button colorButton;
+    public event Action<System.Drawing.Color> ColorChanged;
+    public ColorPalette()
+    {
+        colorButton = new Button
+        {
+            BackColor = Color.White, //Couleur par défault button
+            Width = 100,
+            Height = 100,
+            FlatStyle = FlatStyle.Flat, // Utilisation du style plat (sans ombres ni effets)
+            FlatAppearance = 
+            {
+                BorderSize = 3, // Taille de la bordure
+                BorderColor = Color.Black // Couleur de la bordure (noir)
+            }
+        };
+        
+        int margin = 50;
+        colorButton.FlatAppearance.BorderSize = 0;
+        colorButton.Region = new Region(new Rectangle(0, 0, 100, 100)); // Cercle parfait
+        colorButton.Location = new Point((this.ClientSize.Width - colorButton.Width) / 2, 
+            this.ClientSize.Height - colorButton.Height - margin);
+        
+        //Ajout de l'événement au clic du bouton
+        colorButton.Click += ButtonClick;
+        
+        //Ajoute le bouton à la fenêtre
+        Controls.Add(colorButton);
+    }
+
+    private void ButtonClick(object sender, EventArgs e)
+    {
+        // Afficher la palette de couleurs
+        using (ColorDialog colorDialog = new ColorDialog())
+        {
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Appliquer la couleur choisie au bouton
+                colorButton.BackColor = colorDialog.Color;
+                ColorChanged?.Invoke(colorDialog.Color);
+            }
+        }
+    }
+    
+    
+}
